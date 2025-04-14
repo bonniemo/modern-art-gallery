18b29595ce158d5c16dc8c6095cf2f5bc63002a5
bonniemo <moller.bon@gmail.com>
2025-04-14 15:20:13 +0200

    opening hours and cafe gallery basics

---
+++ app/the-gallery-cafe/OpeningHours.tsx
@@ -0,0 +1,11 @@
+const OpeningHours = () => {
+    return (
+        <section className="uppercase text-center py-16">
+            <h4 className="text-xl">Open all days</h4>
+            <h4 className="text-xl">7am to 3pm</h4>
+            <h4 className="text-m">+ evening events</h4>
+        </section>
+    );
+};
+
+export default OpeningHours;
