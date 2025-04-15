e25a0a70d4f963af820abb86b8e18e4d5e0ed985
bonniemo <moller.bon@gmail.com>
2025-04-15 11:34:12 +0200

    adjusted styles for cafe gallery

--- app/the-gallery-cafe/CafeGallery.tsx
+++ app/the-gallery-cafe/CafeGallery.tsx
@@ -1,39 +1,47 @@
-import Image from "next/image";
-
-import juiceBottles from "@/public/gallery-cafe/desktop/juice-bottles.png";
-import latteCake from "@/public/gallery-cafe/desktop/latte-cake.png";
-
 const CafeGallery = () => {
     return (
-        <section className="flex flex-col pb-28 md:grid md:grid-cols-2 md:grid-rows-2 gap-4">
-            <div className="relative order-1 w-full h-full md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2">
-                <Image
-                    src={latteCake}
-                    alt="Description of the image"
-                    priority
-                    fill
-                    className="object-cover w-full h-auto"
+        <div className="p-4 md:p-6 mt-28 mb-28 lg:mb-46 md:grid md:grid-cols-2 md:gap-4 max-w-6xl m-auto">
+            <picture className="md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3">
+                <source
+                    srcSet="/gallery-cafe/desktop/juice-bottles.png"
+                    media="(min-width: 1000px)"
                 />
-            </div>
-            <section className="bg-dark text-light order-3 px-4 py-12 md:px-6 md:py-6 h-full md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3">
-                <h2 className="uppercase">
+                <source
+                    srcSet="/gallery-cafe/desktop/juice-bottles.png"
+                    media="(min-width: 700px)"
+                />
+                <img
+                    src="/gallery-cafe/desktop/juice-bottles.png"
+                    alt="Glass bottles with plant juice, two with green juice, one with orange and one with red juice"
+                    className="w-full h-full object-cover"
+                />
+            </picture>
+
+            <picture className="md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2">
+                <source
+                    srcSet="/gallery-cafe/desktop/latte-cake.png"
+                    media="(min-width: 1000px)"
+                />
+                <source
+                    srcSet="/gallery-cafe/desktop/latte-cake.png"
+                    media="(min-width: 700px)"
+                />
+                <img
+                    src="/gallery-cafe/desktop/latte-cake.png"
+                    alt="Latte and cake on table"
+                    className="w-full h-full object-cover mt-8 md:mt-0"
+                />
+            </picture>
+            <section className="bg-dark text-light px-6 py-8 md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 md:h-full">
+                <h2 className="uppercase mb-6">
                     SAVOR THE <span className="block">MOMENT</span>
                 </h2>
-                <p className="max-w-[31rem] mt-6 text-sm">
+                <p className="">
                     Relax with a handcrafted coffee, fresh juices, or something
                     sweet as you take in the atmosphere around you.
                 </p>
             </section>
-            <div className="relative w-full h-full md:col-start-2 md:row-start-1 md:row-end-3 order-2">
-                <Image
-                    src={juiceBottles}
-                    alt="glass bottles with plant juice, two with green juice, one with orange and one with red juice"
-                    priority
-                    fill
-                    className="object-cover w-full h-auto"
-                />
-            </div>
-        </section>
+        </div>
     );
 };
 
