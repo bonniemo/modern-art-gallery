52fc50114325f9ddce45c990b06182ba868babd8
bonniemo <moller.bon@gmail.com>
2025-04-23 10:18:34 +0200

    put the section back around link to prevent the mobile layout from breaking

--- app/event-calendar/page.tsx
+++ app/event-calendar/page.tsx
@@ -26,43 +26,44 @@ export default async function EventCalendar() {
                                 <h4 className="text-header-s font-black mt-6 uppercase">
                                     {event.formattedDay}
                                 </h4>
-                                <Link
-                                    href={`/event-calendar/${
-                                        event.id
-                                    }/${slugify(event.title)}`}
-                                    className="w-full shadow-xl md:grid md:grid-cols-3 border border-gray-400 hover:border-black mt-6 p-4"
-                                >
-                                    <div className="relative w-full aspect-[1/1]">
-                                        {event.img && (
-                                            <Image
-                                                src={getCldImageUrl({
-                                                    src: event.img,
-                                                })}
-                                                alt={event.title}
-                                                fill
-                                                className="object-cover"
-                                                priority
-                                            />
-                                        )}
-                                    </div>
-                                    <div className="px-2 md:px-6 md:mt-0 md:col-start-2 col-end-4">
-                                        <h5 className="text-header-xs md:text-header-s font-black">
-                                            {event.title}
-                                        </h5>
-                                        <p className="text-p-s text-gray font-light">
-                                            {formatEventDate(
-                                                event.date.seconds
+                                <section className="w-full shadow-xl md:grid md:grid-cols-3 border border-gray-400 hover:border-black mt-6 p-4">
+                                    <Link
+                                        href={`/event-calendar/${
+                                            event.id
+                                        }/${slugify(event.title)}`}
+                                    >
+                                        <div className="relative w-full aspect-[1/1]">
+                                            {event.img && (
+                                                <Image
+                                                    src={getCldImageUrl({
+                                                        src: event.img,
+                                                    })}
+                                                    alt={event.title}
+                                                    fill
+                                                    className="object-cover"
+                                                    priority
+                                                />
                                             )}
-                                        </p>
-                                        <p className="text-p-s leading-body-s my-6 overflow-hidden text-ellipsis line-clamp-3">
-                                            {event.description}
-                                        </p>
+                                        </div>
+                                        <div className="px-2 md:px-6 md:mt-0 md:col-start-2 col-end-4">
+                                            <h5 className="text-header-xs md:text-header-s font-black">
+                                                {event.title}
+                                            </h5>
+                                            <p className="text-p-s text-gray font-light">
+                                                {formatEventDate(
+                                                    event.date.seconds
+                                                )}
+                                            </p>
+                                            <p className="text-p-s leading-body-s my-6 overflow-hidden text-ellipsis line-clamp-3">
+                                                {event.description}
+                                            </p>
 
-                                        <span className="text-p-s text-gray underline hover:text-black block">
-                                            Read more
-                                        </span>
-                                    </div>
-                                </Link>
+                                            <span className="text-p-s text-gray underline hover:text-black block">
+                                                Read more
+                                            </span>
+                                        </div>
+                                    </Link>
+                                </section>
                             </div>
                         ))}
                     </section>
