d2416c062d3a862794923048cea72f4c869e6203
bonniemo <moller.bon@gmail.com>
2025-04-23 15:22:42 +0200

    added footer to single event page

--- app/event-calendar/[id]/[title]/page.tsx
+++ app/event-calendar/[id]/[title]/page.tsx
@@ -1,4 +1,5 @@
 import { getEventById } from "@/actions/actions";
+import Footer from "@/components/Footer";
 import { formatEventDate } from "@/utils/dateFormatting";
 import { Metadata } from "next";
 import { getCldImageUrl } from "next-cloudinary";
@@ -75,60 +76,63 @@ export default async function EventPage(props: {
     const formattedDate = formatEventDate(event.date.seconds);
 
     return (
-        <main role="main" aria-labelledby="event-title">
-            <section aria-label="Event header image and title">
-                <div className="grid grid-cols-6">
-                    <div className="col-start-1 row-start-1 z-50">
-                        <ButtonBackToCalendar aria-label="Return to event calendar" />
-                    </div>
-                    <div
-                        className="relative w-full aspect-[4/2] col-start-1 col-end-7 row-start-1"
-                        role="img"
-                        aria-label={`Image for ${event.title}`}
-                    >
-                        <Image
-                            src={getCldImageUrl({
-                                src: event.img,
-                            })}
-                            alt={`Featured image for ${event.title}`}
-                            fill
-                            className="object-cover"
-                            priority
-                        />
-                    </div>
-                    <div
-                        className="row-start-1 col-start-4 col-end-7 z-50 text-light flex flex-col items-end p-2"
-                        aria-labelledby="event-title event-date"
-                    >
-                        <h2
-                            id="event-title"
-                            className="max-w-max uppercase text-header-tiny md:text-header-s font-bold"
+        <>
+            <main role="main" aria-labelledby="event-title" className="pb-28">
+                <section aria-label="Event header image and title">
+                    <div className="grid grid-cols-6">
+                        <div className="col-start-1 row-start-1 z-50">
+                            <ButtonBackToCalendar aria-label="Return to event calendar" />
+                        </div>
+                        <div
+                            className="relative w-full aspect-[4/2] col-start-1 col-end-7 row-start-1"
+                            role="img"
+                            aria-label={`Image for ${event.title}`}
                         >
-                            {event.title}
-                        </h2>
-                        <time
-                            id="event-date"
-                            dateTime={new Date(
-                                event.date.seconds * 1000
-                            ).toISOString()}
-                            className="max-w-max text-p-tiny md:text-p-m font-light"
+                            <Image
+                                src={getCldImageUrl({
+                                    src: event.img,
+                                })}
+                                alt={`Featured image for ${event.title}`}
+                                fill
+                                className="object-cover"
+                                priority
+                            />
+                        </div>
+                        <div
+                            className="row-start-1 col-start-4 col-end-7 z-50 text-light flex flex-col items-end p-2"
+                            aria-labelledby="event-title event-date"
                         >
-                            {formattedDate}
-                        </time>
+                            <h2
+                                id="event-title"
+                                className="max-w-max uppercase text-header-tiny md:text-header-s font-bold"
+                            >
+                                {event.title}
+                            </h2>
+                            <time
+                                id="event-date"
+                                dateTime={new Date(
+                                    event.date.seconds * 1000
+                                ).toISOString()}
+                                className="max-w-max text-p-tiny md:text-p-m font-light"
+                            >
+                                {formattedDate}
+                            </time>
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section className="p-4 md:p-8" aria-label="Event details">
-                <h1 className="text-header-s leading-s mt-6 uppercase">
-                    {event.heading}
-                </h1>
-                <p className="text-p-s leading-body-s md:text-p-m md:leading-body-m font-light mt-6">
-                    {event.description}
-                </p>
-                <div className="mt-8">
-                    <ButtonTicket />
-                </div>
-            </section>
-        </main>
+                </section>
+                <section className="p-4 md:p-8" aria-label="Event details">
+                    <h1 className="text-header-s leading-s mt-6 uppercase">
+                        {event.heading}
+                    </h1>
+                    <p className="text-p-s leading-body-s md:text-p-m md:leading-body-m font-light mt-6">
+                        {event.description}
+                    </p>
+                    <div className="mt-8">
+                        <ButtonTicket />
+                    </div>
+                </section>
+            </main>
+            <Footer bgColor="black" />
+        </>
     );
 }
