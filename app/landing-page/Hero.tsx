1f1df092cadb1c5473567eca3ad268d8ea1e09b5
bonniemo <moller.bon@gmail.com>
2025-05-08 13:30:11 +0200

    adjusted responsive styles

--- app/landing-page/Hero.tsx
+++ app/landing-page/Hero.tsx
@@ -11,8 +11,8 @@ const Hero = () => {
             <div className=" absolute top-0 right-0">
                 <Menu />
             </div>
-            <section className="xl:bg-dark xl:w-full xl:h-full col-start-1 col-end-5 row-start-1 row-end-5"></section>
-            <section className="w-full xl:col-start-5 xl:col-end-9 md:col-start-1 md:col-end-7 row-start-1 row-end-5">
+            <section className="lg:bg-dark lg:w-full lg:h-full col-start-1 col-end-5 row-start-1 row-end-5"></section>
+            <section className="w-full lg:col-start-5 lg:col-end-9 md:col-start-1 md:col-end-7 row-start-1 row-end-5">
                 <picture>
                     <source
                         srcSet="/landing-page/desktop/image-hero.jpg"
@@ -29,13 +29,13 @@ const Hero = () => {
                     />
                 </picture>
             </section>
-            <div className="md:col-start-6 md:col-end-13 xl:col-start-2 xl:col-end-7 row-start-2 row-end-3 px-2 md:px-0">
-                <h1 className="mt-8 xl:mt-0 xl:text-light text-header-l leading-l xl:text-header-xl xl:leading-xl xl:mix-blend-difference uppercase">
+            <div className="md:col-start-6 md:col-end-13 lg:col-start-2 lg:col-end-7 lg:flex lg:justify-center row-start-2 row-end-3 px-2 md:px-0">
+                <h1 className="mt-8 lg:mt-0 lg:text-light text-header-l leading-l xl:text-header-xl xl:leading-xl lg:mix-blend-difference uppercase">
                     <span className="block">Modern</span> Art Gallery
                 </h1>
             </div>
-            <section className="mt-8 md:mt-4 xl:mt-0 md:col-start-6 md:col-end-11 lg:col-end-10 xl:col-start-8 xl:col-end-12 md:row-start-3 md:row-end-4 xl:row-start-2 xl:row-end-4 px-2 md:px-0">
-                <p className=" text-dark text-p-s leading-body-s xl:text-p-m xl:leading-body-m xl:max-w-[22rem] font-light">
+            <section className="mt-8 md:mt-4 lg:mt-0 md:col-start-6 md:col-end-11 lg:col-start-8 lg:col-end-12 md:row-start-3 md:row-end-4 lg:row-start-2 lg:row-end-4 px-2 md:px-0">
+                <p className=" text-dark text-p-s leading-body-s xl:text-p-m xl:leading-body-m lg:max-w-[22rem] font-light">
                     The arts in the collection of the Modern Art Gallery all
                     started from a spark of inspiration. Will these pieces
                     inspire you? Visit us and find out.
